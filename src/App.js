09afb53ead7dec9a882556ba77cb6b097101722c
gwongibeom <k201gun@kakao.com>
2023-10-18 19:30:00 +0900

    다이어리 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import DiaryEditer from './DiaryEditer'
+import DiaryList from './DiaryList'
 
 function App() {
+  const dummyList = [
+    {
+      id: 1,
+      author: '권기범',
+      content: '흐애',
+      emotion: 1,
+      created_date: new Date().getTime(),
+    },
+    {
+      id: 2,
+      author: '박진형',
+      content: '최강한화',
+      emotion: 2,
+      created_date: new Date().getTime(),
+    },
+    {
+      id: 3,
+      author: '김태호',
+      content: '맹구',
+      emotion: 3,
+      created_date: new Date().getTime(),
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <DiaryEditer />
+      <DiaryList diaryList={dummyList} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
