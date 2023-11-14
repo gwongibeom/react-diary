f803009352a40b887b271a0995e99c5b7bfab751
gwongibeom <k201gun@kakao.com>
2023-11-14 15:24:13 +0900

    (개발) 수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -21,10 +21,24 @@ const App = () => {
     setData([newItem, ...data])
   }
 
+  const onRemove = (targetId) => {
+    console.log(targetId)
+    const newDiaryList = data.filter((it) => it.id !== targetId)
+    setData(newDiaryList)
+  }
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    )
+  }
+
   return (
     <div className='App'>
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
   )
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,46 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
-const DiaryItem = ({ author, content, emotion, created_date }) => {
+const DiaryItem = ({
+  onRemove,
+  onEdit,
+  id,
+  author,
+  content,
+  emotion,
+  created_date,
+}) => {
+  const [isEdit, setIsEdit] = useState(false)
+
+  const toggleIsEdit = () => {
+    setIsEdit(!isEdit)
+  }
+
+  const [localContent, setLocalContent] = useState(content)
+  const localContentInput = useRef()
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 삭제 하시겠습니까?`)) {
+      onRemove(id)
+    }
+  }
+
+  const handleQuitEdit = () => {
+    setIsEdit(false)
+    setLocalContent(content)
+  }
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus()
+      return
+    }
+
+    if (window.confirm(`${id}번째 일기를 수정 하시겠습니까?`)) {
+      onEdit(id, localContent)
+      toggleIsEdit()
+    }
+  }
+
   return (
     <div className='DiaryItem'>
       <div className='info'>
@@ -10,7 +50,34 @@ const DiaryItem = ({ author, content, emotion, created_date }) => {
         <br />
         <span className='date'>{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className='content'>{content}</div>
+      <div className='content'>
+        {isEdit ? (
+          <>
+            <textarea
+              value={localContent}
+              ref={localContentInput}
+              onChange={(e) => {
+                setLocalContent(e.target.value)
+              }}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          {' '}
+          <button onClick={handleQuitEdit}>수정취소</button>
+          <button onClick={handleEdit}>수정완료</button>
+        </>
+      ) : (
+        <>
+          {' '}
+          <button onClick={toggleIsEdit}>수정하기</button>
+          <button onClick={handleRemove}>삭제하기</button>
+        </>
+      )}
     </div>
   )
 }
