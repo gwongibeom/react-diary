77d5b1a02ebeface917b5c10317b1dcae08ecce2
gwongibeom <k201gun@kakao.com>
2024-02-13 10:48:40 +0900

    콘텍스트 학습 2

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,36 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { memo, useContext, useEffect, useRef, useState } from 'react'
+import { DiaryDispatchContext } from './App'
 
-const DiaryItem = ({
-  onRemove,
-  onEdit,
-  id,
-  author,
-  content,
-  emotion,
-  created_date,
-}) => {
-  useEffect(() => {
-    console.log(`${id}번째 id 렌더`)
-  })
+const DiaryItem = ({ id, author, content, emotion, created_date }) => {
+  const { onRemove, onEdit } = useContext(DiaryDispatchContext)
 
   const [isEdit, setIsEdit] = useState(false)
+  const toggleIsEdit = () => setIsEdit(!isEdit)
 
-  const toggleIsEdit = () => {
-    setIsEdit(!isEdit)
-  }
-
-  const [localContent, setLocalContent] = useState(content)
-  const localContentInput = useRef()
+  const [localContent, setLoclContent] = useState(content)
+  const localContentInput = useRef(null)
 
   const handleRemove = () => {
-    if (window.confirm(`${id}번째 일기를 삭제 하시겠습니까?`)) {
+    if (window.confirm(`${id}번 째 일기를 삭제하시겠습니까?`)) {
       onRemove(id)
     }
   }
 
   const handleQuitEdit = () => {
     setIsEdit(false)
-    setLocalContent(content)
+    setLoclContent(content)
   }
 
   const handleEdit = () => {
@@ -38,52 +26,46 @@ const DiaryItem = ({
       localContentInput.current.focus()
       return
     }
-
-    if (window.confirm(`${id}번째 일기를 수정 하시겠습니까?`)) {
+    if (window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)) {
       onEdit(id, localContent)
       toggleIsEdit()
     }
   }
 
   return (
-    <div className='DiaryItem'>
+    <div className='DiaryItem_container'>
       <div className='info'>
-        <span>
-          작성자: {author} | 감정 : {emotion}
+        <span className='author_info'>
+          | 작성자 : {author} | 감정점수 : {emotion} |
         </span>
         <br />
         <span className='date'>{new Date(created_date).toLocaleString()}</span>
       </div>
+
       <div className='content'>
         {isEdit ? (
-          <>
-            <textarea
-              value={localContent}
-              ref={localContentInput}
-              onChange={(e) => {
-                setLocalContent(e.target.value)
-              }}
-            />
-          </>
+          <textarea
+            ref={localContentInput}
+            value={localContent}
+            onChange={(e) => setLoclContent(e.target.value)}
+          />
         ) : (
-          <>{content}</>
+          content
         )}
       </div>
       {isEdit ? (
-        <>
-          {' '}
-          <button onClick={handleQuitEdit}>수정취소</button>
-          <button onClick={handleEdit}>수정완료</button>
-        </>
+        <div>
+          <button onClick={handleQuitEdit}>수정 취소하기</button>
+          <button onClick={handleEdit}>저장하기</button>
+        </div>
       ) : (
-        <>
-          {' '}
-          <button onClick={toggleIsEdit}>수정하기</button>
+        <div>
           <button onClick={handleRemove}>삭제하기</button>
-        </>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </div>
       )}
     </div>
   )
 }
 
-export default React.memo(DiaryItem)
+export default memo(DiaryItem)
